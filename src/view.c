e139ecc6bf085fbf18c21eb16a2afbce0c15156f
Conner Short <conner.j.short@gmail.com>
2017-12-14 12:49:58 -0800

    Adjusted arrow vertex ordering so shadow looks more correct.

--- src/view.c
+++ src/view.c
@@ -80,7 +80,7 @@ unsigned int arrow_face_vertices[9][7] = {
 	{2, 3, 10, 9},
 	{4, 5, 12, 11},
 	{3, 4, 11, 10},
-	{0, 6, 5, 4, 3, 2, 1}
+	{0, 1, 2, 3, 4, 5, 6}
 };
 
 unsigned int arrow_triangles_arrays[9][9] = {
