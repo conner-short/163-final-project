1608458eacd743621635f6d28a7ee89bf35c28d4
Conner Short <conner.j.short@gmail.com>
2017-12-14 02:27:19 -0800

    Added main.h because it was not comitted last time.

---
+++ include/main.h
@@ -0,0 +1,17 @@
+/**
+ * project: 163 final project
+ *
+ * Conner Short
+ * CMPE 163
+ * December 2017
+ *
+ * main.h: Main program
+**/
+#ifndef _MAIN_H
+#define _MAIN_H
+
+extern double x_translation;
+extern double y_translation;
+extern double z_translation;
+
+#endif
